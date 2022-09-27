7f059d0283583961d6f3cb6430fcc39fcbb849d6
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-09-27 22:11:37 +0530

    new

---
+++ BMI Calculator/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Emmanuel George on 17/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -0,0 +1,55 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Emmanuel George on 17/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    func getBmiValue()->String{
+ 
+        let bmiValue = String(format: "%.1f", bmi?.value ?? 0.0)
+            return bmiValue
+     
+        
+    }
+    func getAdvice()->String{
+ 
+        let advice =  bmi?.advice ?? "Eat pies till u die!"
+            return advice
+    }
+    
+    func getColor()->UIColor{
+ 
+        let color =  bmi?.color ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return color
+    }
+    
+    mutating func calculateBmi(height:Float,weight:Float)  {
+        
+     let bmiValue = weight/(height*height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color:#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)     )
+        }
+        else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color:#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1) )
+        }
+        else{
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color:#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)  )
+        }
+   
+         
+        
+       
+    }
+  
+    
+    
+}
